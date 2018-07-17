e3b97cb225ba939f933f8c78043fff9ae0a7439a
SusanDoggie <susan.doggie@gmail.com>
2018-07-17 10:02:52 +0800

    clean

--- SDSlideMenuViewController.swift
+++ SDSlideMenuViewController.swift
@@ -136,10 +136,6 @@ open class SDSlideMenuViewController: UIViewController, UIGestureRecognizerDeleg
         view.addGestureRecognizer(tapRecongnizer)
     }
     
-    open override var presentedViewController: UIViewController? {
-        return content
-    }
-    
     open override var prefersStatusBarHidden: Bool {
         return content?.prefersStatusBarHidden ?? super.prefersStatusBarHidden
     }
