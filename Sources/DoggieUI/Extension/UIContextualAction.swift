b4695cc21a62006d857ee0cf176beb638d251215
Susan Cheng <>
2019-12-29 15:57:15 +0800

    clean

---
+++ Sources/DoggieUI/Extension/UIContextualAction.swift
@@ -0,0 +1,40 @@
+//
+//  UIContextualAction.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2019 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+@available(iOS 11.0, *)
+extension UIContextualAction {
+    
+    public convenience init(style: UIContextualAction.Style,
+                            title: String?,
+                            image: UIImage? = nil,
+                            backgroundColor: UIColor? = nil,
+                            handler: @escaping (UIContextualAction, UIView) -> Bool) {
+        self.init(style: style, title: title) { action, sourceView, completionHandler in completionHandler(handler(action, sourceView)) }
+        self.image = image
+        self.backgroundColor = backgroundColor
+    }
+}
